9881c89d1a1c7100a319e2f10b992bc7decf55ef
montyholdingdev <155431830+montyholdingdev@users.noreply.github.com>
2024-04-29 17:04:15 +0300

    .

--- front-page.php
+++ front-page.php
@@ -14,14 +14,14 @@ $latest = new WP_Query([
 ]);
 $latest = $latest->posts;
 
-$promoted_post_2 = get_post(45958);
-if ($promoted_post_2) {
-   array_unshift($latest, $promoted_post_2);
-}
-
 $promoted_post = get_post(45551);
 if ($promoted_post) {
    array_unshift($latest, $promoted_post);
+}
+
+$promoted_post_2 = get_post(45958);
+if ($promoted_post_2) {
+   array_unshift($latest, $promoted_post_2);
 }
 
 $technology_all = new WP_Query([
