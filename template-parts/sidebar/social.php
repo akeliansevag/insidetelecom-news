2b006ad17738a8119106c0dc7a12292b841eb9ee
akeliansevag <akelian.sevag@gmail.com>
2026-02-12 10:16:49 +0200

    update social.php

--- template-parts/sidebar/social.php
+++ template-parts/sidebar/social.php
@@ -6,29 +6,40 @@
              <div class="col">
                  <a class="social social-facebook" href="https://www.facebook.com/insidetelecomnews" title="facebook" target="_blank" aria-label="facebook">
                      <i class="ui-facebook"></i>
-                     <span class="social__text">Facebook</span>
-                 </a><!--
-                  --><a class="social social-twitter" href="https://twitter.com/insidetelecom_" title="twitter" target="_blank" aria-label="twitter">
+                     <!-- <span class="social__text">Facebook</span> -->
+                 </a>
+                 <a class="social social-x" href="https://x.com/insidetelecom_" target="_blank" aria-label="twitter">
+                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256" width="18px" height="18px">
+                         <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+                             <g transform="scale(5.12,5.12)">
+                                 <path d="M5.91992,6l14.66211,21.375l-14.35156,16.625h3.17969l12.57617,-14.57812l10,14.57813h12.01367l-15.31836,-22.33008l13.51758,-15.66992h-3.16992l-11.75391,13.61719l-9.3418,-13.61719zM9.7168,8h7.16406l23.32227,34h-7.16406z"></path>
+                             </g>
+                         </g>
+                     </svg>
+                     <!-- <span class="social__text">X</span> -->
+                 </a>
+                 <!-- <a class="social social-twitter" href="https://twitter.com/insidetelecom_" title="twitter" target="_blank" aria-label="twitter">
                      <i class="ui-twitter"></i>
                      <span class="social__text">Twitter</span>
-                 </a><!--
-                  --><a class="social social-youtube" href="https://www.youtube.com/channel/UCAjXVGAApTJCllvep9CuJaA/" title="youtube" target="_blank" aria-label="youtube">
+                 </a> -->
+
+                 <a class="social social-youtube" href="https://www.youtube.com/channel/UCAjXVGAApTJCllvep9CuJaA/" title="youtube" target="_blank" aria-label="youtube">
                      <i class="ui-youtube"></i>
-                     <span class="social__text">Youtube</span>
+                     <!-- <span class="social__text">Youtube</span> -->
                  </a>
              </div>
              <div class="col">
                  <a class="social social-instagram" href="https://www.instagram.com/insidetelecom.news/?hl=en" title="instagram" target="_blank" aria-label="instagram">
                      <i class="ui-instagram"></i>
-                     <span class="social__text">Instagram</span>
+                     <!-- <span class="social__text">Instagram</span> -->
                  </a>
                  <a class="social social-linkedin" href="https://www.linkedin.com/company/inside-telecom/" title="rss" target="_blank" aria-label="rss">
                      <i class="ui-linkedin"></i>
-                     <span class="social__text">LinkedIn</span>
+                     <!-- <span class="social__text">LinkedIn</span> -->
                  </a>
                  <a class="social social-whatsapp" href="https://whatsapp.com/channel/0029Vb7OJuOGzzKZ2ZyDH60G" title="rss" target="_blank" aria-label="rss">
                      <i class="ui-whatsapp"></i>
-                     <span class="social__text">whatsapp</span>
+                     <!-- <span class="social__text">whatsapp</span> -->
                  </a>
              </div>
          </div>
